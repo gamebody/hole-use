073fc80175780fc35f733b835ff064d4d24d1d56
gamebody <liuyun@jindankeji.com>
2020-02-12 17:21:03 +0800

    tslint

--- src/useLocalStorage.ts
+++ src/useLocalStorage.ts
@@ -1,24 +1,21 @@
 import { useEffect, useState } from 'react';
 
-
 const useLocalStorage = <T>(key: string, initialValue?: T): [T, React.Dispatch<T>] => {
-
   const [state, setState] = useState<T>(() => {
-    const localStorageValue = localStorage.getItem(key)
+    const localStorageValue = localStorage.getItem(key);
 
     if (localStorageValue) {
       return JSON.parse(localStorageValue);
     } else {
       return initialValue;
     }
-  })
+  });
 
   useEffect(() => {
     localStorage.setItem(key, JSON.stringify(state));
-  }, [state])
+  }, [state]);
 
-
-  return [state, setState]
-}
+  return [state, setState];
+};
 
 export default useLocalStorage;
